e515cc41f799d0f136e2bbffc25597714f881f40
João Camelo <1140295@isep.ipp.pt>
2018-11-30 20:38:20 +0000

    Warehouse select template Warehouse details template Item details template warehouse inventory template

---
+++ app/src/main/java/pt/simov/stockit/ItemActivity.java
@@ -0,0 +1,13 @@
+package pt.simov.stockit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
