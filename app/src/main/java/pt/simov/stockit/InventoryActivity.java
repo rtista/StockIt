d48f2f6092bf30c02b56180b8692a1143ea26de3
1140279AndreDias <1140279@isep.ipp.pt>
2018-12-28 17:29:56 +0000

    Added Option Menu to InventoryActivity

--- app/src/main/java/pt/simov/stockit/InventoryActivity.java
+++ app/src/main/java/pt/simov/stockit/InventoryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -51,6 +52,12 @@ public class InventoryActivity extends AppCompatActivity {
             }
         });
         lv.setSelection(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.inventory_optionsmenu, menu);
+        return true;
     }
 
     @Override
