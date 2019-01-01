06082fcfe45fa46d4eee7ee81bcad3a93a60e681
1140279AndreDias <1140279@isep.ipp.pt>
2019-01-01 20:21:02 +0000

    Added BarcodeActivity

--- app/src/main/java/pt/simov/stockit/InventoryActivity.java
+++ app/src/main/java/pt/simov/stockit/InventoryActivity.java
@@ -64,13 +64,17 @@ public class InventoryActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {// Handle item selection
         switch (item.getItemId()) {
             case R.id.iom_add:
-                Intent i=new Intent(this,ItemActivity.class);
+                Intent i = new Intent(this,ItemActivity.class);
                 i.putExtra("NAME", "");
                 i.putExtra("DESCRIPTION", "");
                 i.putExtra("QUANTITY", "");
                 i.putExtra("MIN_QUANTITY", "");
                 i.putExtra("REQUEST_CODE",3);
                 startActivity(i);
+                return true;
+            case R.id.iom_barcode:
+                Intent iBarcode = new Intent(this,BarcodeActivity.class);
+                startActivity(iBarcode);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
